019d001478f8a31ea4edd315b7064c1c410cf1cc
limahrc <limahrc@gmail.com>
2017-07-08 21:56:45 -0300

    correção da função main

--- src/main.c
+++ src/main.c
@@ -1,5 +1,65 @@
 #include "caixa.h"
 
+void troca(char *x, char *y);
+void permuta(Caixa *caixa, Bola *init);
+int fatorial(int n);
+
+int main() {
+
+  Caixa *S = NULL, *aux = NULL;
+  int i, j, possib=0, cxs, bol;
+  char ltr, opt, sair, cor[20];
+
+    do{
+      system("clear");
+      printf(":::::::::\tPERMUTA GAME\t:::::::::\n");
+      printf("Quantas caixas serão? "); scanf("%i", &cxs);
+      for (i=0; i<cxs; i++){
+          printf("\nC%i: Qual será a cor das bolas nesta caixa? ", i+1);
+          setbuf(stdin, NULL);
+          scanf("%s", cor);
+
+          printf("Quantas bolas serão? "); scanf("%i", &bol);
+          possib+=bol; //Soma do número de bolas
+          aux = novaCaixa(bol);
+
+          for (j=0; j<bol; j++){
+            printf("Digite a letra da %iª bola: ", j+1);
+            getchar(); scanf("%c", &ltr);
+            insereNaCaixa(aux, novaBola(cor, ltr));
+          }
+
+          ligaCaixa(&S, aux); //Faz o encadeamento ordenado das caixas
+      }
+      printf("\nExistem %i possibilidades de resultados para este jogo.\n",
+       fatorial(possib));
+
+      do{
+        printf("Deseja exibir todas as permutações? (s/n) "); getchar();
+        scanf("%c", &opt);
+        if(opt == 's' || opt == 'S'){
+          printf("\n");
+          permuta(S, aux->primeira);
+          break;
+        }
+        else if(opt == 'n' || opt == 'N') {
+          printf("Ok.\n");
+          break;
+        }
+        else printf("\nOpção inválida. Tente novamente. ");
+    } while(opt);
+
+    printf("\nDeseja jogar novamente? (s/n) "); getchar();
+    scanf("%c", &sair);
+
+    if(sair == 's' || sair == 'S');
+    else if(sair == 'n' || sair == 'N') break;
+    else printf("\nOpção inválida. Tente novamente. ");
+  } while(sair);
+    printf("\n\nMuito obrigado!\nPrograma encerrado.\n");
+  return 0;
+}
+
 void troca(char *x, char *y){
   char temp;
   temp = *x;
@@ -27,47 +87,7 @@ void permuta(Caixa *caixa, Bola *init){
   }
 }
 
-int main(int argc, char const *argv[]) {
-  Caixa* S = NULL;
-  Caixa* aux = NULL;
-  int i, j, cxs, bol;
-  char ltr, cor[20];
-  system("clear");
-  printf("Quantas caixas? "); scanf("%i", &cxs);
-  for (i=0; i<cxs; i++){
-      printf("\nC%i: Qual será a cor das bolas nesta caixa? ", i+1);
-      setbuf(stdin, NULL);
-      scanf("%s", cor);
-      printf("Quantas bolas serão? "); scanf("%i", &bol);
-      aux = novaCaixa(bol);
-      for (j=0; j<bol; j++){
-        printf("Digite a letra da %iª bola: ", j+1);
-        getchar(); scanf("%c", &ltr);
-        insereNaCaixa(aux, novaBola(cor, ltr));
-      }
-      ligaCaixa(&S, aux);
-  }
-
-<<<<<<< HEAD
-  exibeCaixa(S);
-=======
-  C1 = novaCaixa(3);
-  insereNaCaixa(C1, novaBola("Verde", 'J'));
-  insereNaCaixa(C1, novaBola("Verde", 'G'));
-
-  aux = novaCaixa(1);
-  insereNaCaixa(aux, novaBola("Azul", 'U'));
-  insereNaCaixa(aux, novaBola("Azul", 'R'));
-
-  Caixa *C2 = novaCaixa(4);
-  insereNaCaixa(C2, novaBola("Laranja", 'B'));
-  insereNaCaixa(C2, novaBola("Laranja", 'Y'));
-
-  ligaCaixa(&C1, C2);
-  ligaCaixa(&C1, aux);
-
-
-  exibeCaixa(C1);
->>>>>>> 26016437354d8e86c2a1636c2b00dd3eb801e3c2
-  return 0;
+int fatorial(int n){
+  if(n==0 || n==1) return 1;
+  else return n*fatorial(n-1);
 }
